670f3844f9126a7e7729b5b0ccad07cf420ec258
Jian Yang Lee <leejyang00@gmail.com>
2024-12-29 23:19:18 +1000

    2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="text-center text-xs text-gray-400 py-8">
-      <div className="">© 2024 Jian Yang Lee. All Rights Reserved.</div>
+      <div className="">© 2025 Jian Yang Lee. All Rights Reserved.</div>
     </div>
   );
 }
