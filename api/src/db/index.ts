f787f236a790042a9d53d656d5b60f874c5705f5
Jian Yang Lee <leejyang00@gmail.com>
2025-03-10 16:31:32 +1000

    simplifying db index;

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,45 +1,18 @@
-// import { drizzle } from 'drizzle-orm/postgres-js'
-// import postgres from 'postgres'
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { Context } from "hono";
 
-// const connectionString = process.env.DATABASE_URL || "empty";
-// console.log('connectionString-env', connectionString);
+export type Bindings = {
+  SUPABASE_URL: string;
+  SUPABASE_KEY: string;
+}
 
-// // Disable prefetch as it is not supported for "Transaction" pool mode
-// const client = postgres(connectionString, { prepare: false })
-// const db = drizzle(client);
+export function getSupabaseClient(c: Context<{ Bindings: Bindings }>): SupabaseClient {
+  const supabaseUrl = c.env.SUPABASE_URL;
+  const supabaseKey = c.env.SUPABASE_KEY;
 
-// export default db;
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Supabase credentials not found');
+  }
 
-// // const allUsers = await db.select().from(users);
-      
-
-// import { createClient } from '@supabase/supabase-js';
-
-// Initialize Supabase client
-// const supabaseUrl = process.env.SUPABASE_URL!;
-// const supabaseKey = process.env.SUPABASE_KEY!;
-// const supabase = createClient(supabaseUrl, supabaseKey);
-
-// export default supabase;
-
-// interface Env {
-//   SUPABASE_URL: string;
-//   SUPABASE_KEY: string;
-// }
-
-// export default {
-//   async fetch(request: Request, env: Env) {
-//     console.log('env-URL', env.SUPABASE_URL);
-//     const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
-//     const { data, error } = await supabase.from("countries").select('*');
-
-//   }
-// }
-    // if (error) throw error;
-//     return new Response(JSON.stringify(data), {
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     });
-//   },
-// };
+  return createClient(supabaseUrl, supabaseKey);
+}
--- api/src/routes/books.ts
+++ api/src/routes/books.ts
@@ -1,25 +1,10 @@
 import { Hono } from "hono";
-import { createClient } from "@supabase/supabase-js";
-
-type Bindings = {
-  SUPABASE_URL: string;
-  SUPABASE_KEY: string;
-}
+import { getSupabaseClient, Bindings } from "@/db";
 
 export const booksRoute = new Hono<{ Bindings: Bindings }>();
 
 booksRoute.get("/", async (c) => {
-  const supabaseUrl = c.env.SUPABASE_URL
-  const supabaseKey = c.env.SUPABASE_KEY
-
-  console.log('supabaseUrl', supabaseUrl);
-  console.log('supabaseKey', supabaseKey);
-
-  if (!supabaseUrl || !supabaseKey) {
-    return c.json({ error: 'Supabase credentials not found' }, 500)
-  }
-
-  const supabase = createClient(supabaseUrl, supabaseKey)
+  const supabase = getSupabaseClient(c);
   
   let { data: books_table, error } = await supabase
   .from('books_table')
