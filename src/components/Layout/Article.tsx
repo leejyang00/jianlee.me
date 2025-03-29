a15806cb447c85eb4df13a09f2823a666aadbb91
Jian Yang Lee <leejyang00@gmail.com>
2025-03-29 17:20:09 +1000

    with home page

--- src/components/Layout/Article.tsx
+++ src/components/Layout/Article.tsx
@@ -13,7 +13,7 @@ function Article({ title, date, children }: ArticleProps) {
     <div className="max-w-xl m-auto px-6">
       {/* Navigation Section */}
       <div className="flex flex-row items-center justify-start py-4 gap-1">
-        <Link to="/works" className="text-mint dark:text-pink hover:underline hover:underline-offset-4 font-apple">Works</Link>
+        <Link to="/" className="text-mint dark:text-pink hover:underline hover:underline-offset-4 font-apple">Home</Link>
         <NavigateNextIcon sx={{ fontSize: 18 }} />
         <h2 className="font-semibold text-lg">{title}</h2>
         <div className="ml-2 text-xs font-bold px-1 bg-slate-300 dark:bg-slate-600 rounded-sm">{date}</div>
